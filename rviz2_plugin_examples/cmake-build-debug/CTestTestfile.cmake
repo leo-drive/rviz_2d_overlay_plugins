b908690d764a479f71c9186446380b884438ea8c
Barış Zeren <bzeren1819@gmail.com>
2023-09-05 16:15:03 +0300

    Fix plugins before init

---
+++ rviz2_plugin_examples/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bzeren/projects/rviz_plugin_ws/src/rviz_2d_overlay_plugins/rviz2_plugin_examples
+# Build directory: /home/bzeren/projects/rviz_plugin_ws/src/rviz_2d_overlay_plugins/rviz2_plugin_examples/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
